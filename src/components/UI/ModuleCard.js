9cf762fd2dfe293757fcf869612a46ff31d13fee
GraemeAngusJones <86078388+GraemeAngusJones@users.noreply.github.com>
2022-06-12 23:42:52 +0100

    No clearer, I'm afraid!

--- src/components/UI/ModuleCard.js
+++ src/components/UI/ModuleCard.js
@@ -1,3 +1,4 @@
+import OnHover from './OnHover.js';
 import { CardWithFavourite } from './Card.js';
 import ToolTip from './ToolTip.js';
 import { ActionTray, ActionSubscribe, ActionUnsubscribe, ActionModify, ActionDelete } from './Actions.js';
@@ -11,45 +12,47 @@ export default function ModuleCard({module,handlers}) {
   // Methods -------------------------------------
   // View ----------------------------------------
   return (
-    <CardWithFavourite
-      isFavourite={module.isSubscribed}
-      unFavourite={() => handlers.handleUnsubscribe(module.ModuleID)}
-    >
-      <div className="cardLayout">
+    <OnHover>
+      <CardWithFavourite
+        isFavourite={module.isSubscribed}
+        unFavourite={() => handlers.handleUnsubscribe(module.ModuleID)}
+      >
+        <div className="cardLayout">
 
-        <div className="cardImage">
-          <img src={module.ModuleImage} alt="Visual representation of module" />
-        </div>
+          <div className="cardImage">
+            <img src={module.ModuleImage} alt="Visual representation of module" />
+          </div>
 
-        <div className="cardDetails">
-          <h1>{module.ModuleName} ({module.ModuleCode})</h1>
-          <p>
-            <span className="cardAttribute">Level</span>
-            <span className="cardValue">{module.ModuleLevel}</span>
-          </p>
-        </div>
+          <div className="cardDetails">
+            <h1>{module.ModuleName} ({module.ModuleCode})</h1>
+            <p>
+              <span className="cardAttribute">Level</span>
+              <span className="cardValue">{module.ModuleLevel}</span>
+            </p>
+          </div>
 
-        <div className="cardActions">
-          <ActionTray>
-            {module.isSubscribed
-              ?
-              <ToolTip message="Click to remove module to favourites">
-                <ActionUnsubscribe onClick={() => handlers.handleUnsubscribe(module.ModuleID)} />
+          <div className="cardActions">
+            <ActionTray>
+              {module.isSubscribed
+                ?
+                <ToolTip message="Click to remove module to favourites">
+                  <ActionUnsubscribe onClick={() => handlers.handleUnsubscribe(module.ModuleID)} />
+                </ToolTip>
+                :
+                <ToolTip message="Click to add module to favourites">
+                  <ActionSubscribe onClick={() => handlers.handleSubscribe(module.ModuleID)} />
+                </ToolTip>
+              }
+              <ToolTip message="Click to modify module details">
+                <ActionModify onClick={handlers.handleModify} />
               </ToolTip>
-              :
-              <ToolTip message="Click to add module to favourites">
-                <ActionSubscribe onClick={() => handlers.handleSubscribe(module.ModuleID)} />
+              <ToolTip message="Delete module from list">
+                <ActionDelete onClick={() => handlers.handleDelete(module.ModuleID)} />
               </ToolTip>
-            }
-            <ToolTip message="Click to modify module details">
-              <ActionModify onClick={handlers.handleModify} />
-            </ToolTip>
-            <ToolTip message="Delete module from list">
-              <ActionDelete onClick={() => handlers.handleDelete(module.ModuleID)} />
-            </ToolTip>
-          </ActionTray>
+            </ActionTray>
+          </div>
         </div>
-      </div>
-    </CardWithFavourite>
+      </CardWithFavourite>
+    </OnHover>
   );
 }
