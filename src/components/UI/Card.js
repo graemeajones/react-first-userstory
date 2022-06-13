7c0ef60cb2f27c919fe274215282eae61e856a30
Graeme Jones <48164351+graemeajones@users.noreply.github.com>
2022-06-13 10:36:25 +0100

    Tooltip now uses OnHover rather than its own separate state (#2)
    Co-authored-by: GraemeAngusJones <86078388+GraemeAngusJones@users.noreply.github.com>

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,5 +1,5 @@
-import OnHover from './OnHover.js';
 import { IconRedHeart } from './Icons.js';
+import OnHover from './OnHover.js';
 import ToolTip from './ToolTip.js';
 import './Card.css';
 
@@ -25,21 +25,6 @@ export function Card({ children, onClick, isParentHovering }) {
   );
 }
 
-export function CardWithHover({ children, onClick }) {
-  // Properties ----------------------------------
-  // Hooks ---------------------------------------
-  // Context -------------------------------------
-  // Methods -------------------------------------
-  // View ----------------------------------------
-  return (
-    <OnHover>
-      <Card onClick={onClick}>
-        {children}
-      </Card>
-    </OnHover>
-  );
-}
-
 export function CardWithFavourite({ children, onClick, isFavourite, unFavourite, isParentHovering }) {
   // Properties ----------------------------------
   // Hooks ---------------------------------------
@@ -51,7 +36,9 @@ export function CardWithFavourite({ children, onClick, isFavourite, unFavourite,
       {
         isFavourite && (
           <div className="favourite" onClick={unFavourite}>
-            <ToolTip message="Click to unfavourite"><IconRedHeart /></ToolTip>
+            <OnHover>
+              <ToolTip message="Click to unfavourite"><IconRedHeart /></ToolTip>
+            </OnHover>
           </div>
         )
       }
--- src/components/UI/ToolTip.js
+++ src/components/UI/ToolTip.js
@@ -3,13 +3,13 @@ import { Card } from './Card.js';
 import './ToolTip.css';
 
 
-export default function ToolTip({ children, message }) {
+export default function ToolTip({ children, message, isParentHovering }) {
   // Properies -----------------------------------
   // let widthToolTip = 36; // Initially, the width value of the closed Action
   let widthMessage = 100; // Initially, the min-width value of ToolTip message
 
   // Hooks ---------------------------------------
-  const [isHovering, setIsHovering] = useState(false);
+  const [isShown, setIsShown] = useState(false);
   const refToolTip = useRef(null);
   const refMessage = useRef(null);
   
@@ -17,33 +17,31 @@ export default function ToolTip({ children, message }) {
   }, [refToolTip,refMessage]);
 
   // Methods -------------------------------------
-  let startToolTipTimer; // Don't start ToolTip straight away
-  let stopToolTipTimer; // Clear lingering ToolTips!
+  let startTimer; // Don't start ToolTip straight away
+  let stopTimer; // Clear lingering ToolTips!
   const delayStartOfToolTip = 333; // i.e. millisecs
   const maxDurationOfToolTip = 2000; // i.e. two seconds
 
   const startToolTip = () => {
-    startToolTipTimer = setTimeout( () => setIsHovering(true), delayStartOfToolTip );
-    stopToolTipTimer = setTimeout( () => setIsHovering(false), maxDurationOfToolTip );
+    startTimer = setTimeout( () => setIsShown(true), delayStartOfToolTip );
+    stopTimer = setTimeout( () => setIsShown(false), maxDurationOfToolTip );
   }
 
   const clearToolTip = () => {
-    clearTimeout(startToolTipTimer);
-    clearTimeout(stopToolTipTimer);
-    setIsHovering(false);
+    clearTimeout(startTimer);
+    clearTimeout(stopTimer);
+    setIsShown(false);
   }
 
   // View ----------------------------------------
+  if (isParentHovering && !isShown) startToolTip();
+  if (!isParentHovering && isShown) clearToolTip();
+
   return (
-    <div
-      className="ToolTip"
-      onMouseEnter={startToolTip}
-      onMouseLeave={clearToolTip}
-      ref={refToolTip}
-    >
+    <div className="ToolTip" ref={refToolTip}>
       {children}
       {
-        isHovering && (
+        isShown && (
           <Card>
             <div
               className="ToolTipMessage"
